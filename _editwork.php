3de341c631c147fce993b58e831236196e0ebe6b
chanyanuch <ohzunk@MacBook-Pro-khxng-chanyanuch.local>
2021-01-31 23:28:06 +0700

    update update worktime allday

--- _editwork.php
+++ _editwork.php
@@ -5,7 +5,6 @@
     $day_id = $_POST['e_day'];
     $shift = $_POST['e_shift'];
     $dentist_id = $_POST['e_dentist_name'];
-
     if($day_id == 1){
         $day_name = 'วันจันทร์';
     }else if($day_id == 2){
@@ -20,6 +19,16 @@
         $day_name = 'วันเสาร์';
     }else if($day_id == 7){
         $day_name = 'วันอาทิตย์';
+    }
+
+    $sql_query = "INSERT INTO `master_worktime`(`day_id`, `day_name`, `shift`, `dentist_id`) VALUES ";
+    if($shift == 'ทั้งวัน')
+    {
+        $result = $conn->query("DELETE FROM master_worktime WHERE work_id = '$worktime'");
+        $sql_query.="('$day_id','$day_name','เช้า','$dentist_id'),";
+        $sql_query.="('$day_id','$day_name','บ่าย','$dentist_id'),";
+        $sql_query = substr($sql_query, 0, -1);
+        $result = $conn->query($sql_query);
     }
 
     $result = $conn->query("UPDATE `master_worktime` SET 
