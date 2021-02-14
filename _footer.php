8d9012ceb3bd24dea22fe08d485cfa5d2eeb7ca3
chanyanuch <ohzunk@MacBook-Pro-khxng-chanyanuch.local>
2021-02-14 23:53:35 +0700

    add

--- _footer.php
+++ _footer.php
@@ -1,15 +1,10 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2020-2020 <a href="http://adminlte.io">Dental Clinic</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 0.0.1
-    </div>
-</footer>
+<!-- <footer class="main-footer">
+</footer> -->
 
 <!-- Control Sidebar -->
-<aside class="control-sidebar control-sidebar-dark">
+<!-- <aside class="control-sidebar control-sidebar-dark"> -->
     <!-- Control sidebar content goes here -->
-</aside>
+<!-- </aside> -->
 <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
